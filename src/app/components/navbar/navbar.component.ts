b68cab6d6cdcf20e9156528519857a8e8bda7c0d
ProgramistaJarek <dojarka0@gmail.com>
2022-08-16 12:28:05 +0200

    dashboard page with authorization

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.scss'],
 })
-export class NavbarComponent implements OnInit {
+export class NavbarComponent {
+  constructor(private service: UserService) {}
 
-  constructor() { }
+  isLoggedin = this.service.isUserLoggedIn;
 
-  ngOnInit(): void {
+  onLogout() {
+    this.service.logout();
   }
-
 }
