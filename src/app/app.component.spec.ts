3733a388fd284ded0302b9a896ab5eff713c4402
ProgramistaJarek <dojarka0@gmail.com>
2022-08-30 13:50:18 +0200

    test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,13 +1,28 @@
-import { TestBed } from '@angular/core/testing';
+import { Component } from '@angular/core';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { Router } from '@angular/router';
+
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+  let router: Router;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [RouterTestingModule],
-      declarations: [AppComponent],
+      imports: [RouterTestingModule.withRoutes([])],
+      declarations: [AppComponent, NavbarStubComponent],
     }).compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+    router = TestBed.get(Router);
+    router.initialNavigation();
   });
 
   it('should create the app', () => {
@@ -21,4 +36,13 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app.title).toEqual('forms');
   });
+
+  it('navigates to another router', () => {
+    router.navigate(['']);
+    expect(component).toBeDefined();
+    expect(router.url).toBe('/');
+  });
 });
+
+@Component({ selector: 'app-navbar', template: '' })
+class NavbarStubComponent {}
